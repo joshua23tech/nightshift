840ca7fcca879fa95bd3928df8c5f516fcaf0c4d
joshua23tech <joshua23.tech@gmail.com>
2025-02-15 23:24:18 +0000

    done done done

--- efforts_log.py
+++ efforts_log.py
@@ -1,2 +1,4 @@
 print("Today I have worked on list comprehensions in Python towards my interview - 13/02/25")
 print("Further work completed on list comprehensions in Python towards my interview - 14/02/25")
+print("Finished all exercises on list comprehensions, dictionary comprehensions and set comprehensions; then completed sets and tuple exclusive exercises")
+print("Also reviewed core concepts such as loops, functions and variables")
