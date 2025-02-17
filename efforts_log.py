4b45785d43aa5b933e96ae8590f7329c08dfd054
joshua23tech <joshua23.tech@gmail.com>
2025-02-17 04:13:34 +0000

    still working

--- efforts_log.py
+++ efforts_log.py
@@ -3,3 +3,4 @@ print("Further work completed on list comprehensions in Python towards my interv
 print("Finished all exercises on list comprehensions, dictionary comprehensions and set comprehensions; then completed sets and tuple exclusive exercises")
 print("Also reviewed core concepts such as loops, functions and variables")
 print("I completed a mixture of logic problems and practice questions in Python; these short problems covered a range of topics including loops, comprehensions, and string manipulation - 16/02/25")
+print("More logic problems and practice questions in Python; learned how to work with the ord() and char() functions to parse back and forth between corresponding ASCII values - 17/02/25")
