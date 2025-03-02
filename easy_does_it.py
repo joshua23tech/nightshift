5e067b612fe6e2a2acf01cb1f150bae44aa912d7
joshua23tech <joshua23.tech@gmail.com>
2025-03-02 23:49:20 +0000

    keeping limber

---
+++ easy_does_it.py
@@ -0,0 +1,7 @@
+str_nba_next_gen = ["Anthony Edwards", "Paolo Banchero", "Cooper Flag", "Amen Thompson"]
+
+str_nba_next_gen_all_stars = [f"{nba_next_gen} has been nominated for his first all star appearance!" for nba_next_gen in str_nba_next_gen]
+
+for all_star in str_nba_next_gen_all_stars:
+    print(all_star)
+
